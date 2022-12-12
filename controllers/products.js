adc04f23e87613273da91553b5920d652e0a663a
Dharmik Patel <dharmikpatel.pepalla@gmail.com>
2022-12-12 20:29:36 +0530

    searching added

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,13 @@
 const Product=require('../models/products')
 const getAllProducts = async (req, res) => {
-    const products=await Product.find()
+    const products=await Product.find(req.query)
     
     res.status(201).json({products})
 }
 
 const getAllProductsTesting = async(req,res) => {
-    res.status(201).json({msg:'all testing products are getting'})
+    const products = await Product.find()
+    res.status(201).json({ products })
 }
 
 module.exports = { getAllProducts, getAllProductsTesting };
